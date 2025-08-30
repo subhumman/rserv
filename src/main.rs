87de993ae78d05b9d92a321d4ac4554ebabdf5d4
Den <denis02012000@hotmail.com>
2025-08-31 00:25:42 +0300

    master/end single-thread simple serv. Next morning i will end all multi-thread serv with ssl. Just wait me

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 
-use std::net::TcpListener;
+use std::fs;
+use std::net::{TcpListener, TcpStream};
+use std::io::{BufReader, prelude::*}; // импортировать все из пакета prelude
 
-#[allow(unused_variables)]
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        println!("соединение установленно!. ");
+        con_handle(&stream);
     }
+}
+
+#[allow(unused_variables)]
+fn con_handle(mut stream: &TcpStream){
+    let buf_reader = BufReader::new(stream);
+    let requ = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status, filename) = if requ == "GET / HTTP/1.1"{
+        ("HTTP/1.1 200 OK", "darkprince.html")
+    }else{
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(&filename).unwrap();
+    let len = content.len();
+    let response = format!("{status}\r\nContent-Length: {len}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
